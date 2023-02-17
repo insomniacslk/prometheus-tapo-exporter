c36581634d8b3660050e5253d4b386a02787d26e
Andrea Barberio <insomniac@slackware.it>
2023-02-17 12:50:02 +0100

    Added cmd/unifi-list-tapo
    Signed-off-by: Andrea Barberio <insomniac@slackware.it>

---
+++ cmd/unifi-list-tapo/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/unpoller/unifi"
+)
+
+var (
+	flagUsername = flag.String("u", "", "Unifi controller username")
+	flagPassword = flag.String("p", "", "Unifi controller password")
+	flagURL      = flag.String("U", "http://127.0.0.1:8443", "Unifi controller URL")
+	flagSiteName = flag.String("s", "default", "Site name")
+	flagOutfile  = flag.String("o", "tapo_devices.txt", "Output file")
+)
+
+func main() {
+	flag.Parse()
+	c := unifi.Config{
+		User:     *flagUsername,
+		Pass:     *flagPassword,
+		URL:      *flagURL,
+		ErrorLog: log.Printf,
+		DebugLog: nil,
+	}
+	uni, err := unifi.NewUnifi(&c)
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+	sites, err := uni.GetSites()
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+	siteIdx := -1
+	for idx, site := range sites {
+		if site.Name == *flagSiteName {
+			siteIdx = idx
+		}
+	}
+	if siteIdx == -1 {
+		log.Fatalf("Site '%s' not found", *flagSiteName)
+	}
+	clients, err := uni.GetClients([]*unifi.Site{sites[siteIdx]})
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+	var output string
+	idx := 0
+	for _, client := range clients {
+		if client.Name == "p110" || strings.Contains(client.Name, "tapo") {
+			// NOTE: these device names only get Tapo P100 and P110 plugs
+			fmt.Println(idx+1, client.ID, client.Hostname, client.IP, client.Name, client.Mac, client.LastSeen)
+			output += fmt.Sprintf("%s\n", client.IP)
+			idx++
+		}
+	}
+	// NOTE: output is unsorted
+	if err := os.WriteFile(*flagOutfile, []byte(output), 0644); err != nil {
+		log.Fatalf("Failed to write to file '%s': %v", *flagOutfile, err)
+	}
+	fmt.Printf("Written to file '%s'\n", *flagOutfile)
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/insomniacslk/tapo v0.0.0-20221025193547-7adf95f844cb
 	github.com/insomniacslk/xjson v0.0.0-20210106140854-1589ccfd1a1a
 	github.com/prometheus/client_golang v1.13.0
+	github.com/unpoller/unifi v0.3.2
 )
 
 require (
@@ -18,6 +19,7 @@ require (
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.37.0 // indirect
 	github.com/prometheus/procfs v0.8.0 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 )
